cb72b7660ead345c04c00680668afb494d3985a9
grimbn <bhasin.agrim996@gmail.com>
2020-10-21 19:09:13 +0530

    S4_AA_Rotation_1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 5f, ySpeed = 5f;
+    [SerializeField] float horizontalRange = 5f, yMin = -3f, yMax = 3f;
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        MoveShip();
+        RotateShip();
+    }
+
+    private void MoveShip()
+    {
+        float xOffset = Input.GetAxis("Horizontal") * xSpeed * Time.deltaTime;
+        float yOffset = Input.GetAxis("Vertical") * ySpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float xPos = Mathf.Clamp(rawXPos, -horizontalRange, +horizontalRange);
+        float yPos = Mathf.Clamp(rawYPos, yMin, yMax);
+
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z); 
+        //Debug.Log(xOffset + "," + (1f/Time.deltaTime));
+    }
+
+    private void RotateShip()
+    {
+        float pitch = 0f;
+        float yaw = 0f;
+        float roll = 0f;
     }
 }

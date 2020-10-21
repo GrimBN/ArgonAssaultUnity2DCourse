01f8a54d5df02243f49c23684f045bf98c872746
grimbn <bhasin.agrim996@gmail.com>
2020-10-22 01:54:19 +0530

    S4_AA_Full ship movement+Particles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 5f, ySpeed = 5f;
-    [SerializeField] float horizontalRange = 5f, yMin = -3f, yMax = 3f;
+    [Tooltip("In m")][SerializeField] float horizontalRange = 5f, yMin = -3f, yMax = 3f;
+    [SerializeField] float positionPitchFactor = -7f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+    [Tooltip("Small value to correct apparent roll due to perspective")][SerializeField] float positionRollFactor = 2f;
 
     void Start()
     {
@@ -35,8 +40,16 @@ public class Player : MonoBehaviour
 
     private void RotateShip()
     {
-        float pitch = 0f;
-        float yaw = 0f;
-        float roll = 0f;
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = Input.GetAxis("Vertical") * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControl;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToPerspectiveCorrection = transform.localPosition.x * transform.localPosition.y * positionRollFactor;
+        float roll = Input.GetAxis("Horizontal") * controlRollFactor + rollDueToPerspectiveCorrection;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }

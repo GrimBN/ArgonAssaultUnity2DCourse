d8eef89c9a45bbb8f0f8434ac6538470fc1fa776
grimbn <bhasin.agrim996@gmail.com>
2020-10-25 03:53:42 +0530

    S4_AA_Enemy Explosion Effects

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform explosionParent;
+    BoxCollider boxCollider;
+
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        if (GetComponent<BoxCollider>() == null)
+        {
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+            boxCollider.isTrigger = false;
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity, explosionParent);                
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField] float destructionDelay = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, destructionDelay);
+    }
+}
